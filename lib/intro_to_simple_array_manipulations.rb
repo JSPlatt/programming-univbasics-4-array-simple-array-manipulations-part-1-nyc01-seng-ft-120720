06b5cf984c4d79883436f9623e0797bf2db0a033
Jeffrey Platt <jsplatt@gmail.com>
2020-11-24 18:14:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -21,20 +21,3 @@ end
 def shift_with_args(array)
   array.shift(2)
 end
-
-
-
-
-
-
-
-
-#def using_pop(array)
-#continents = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
-#continents.pop
-#end
-
-#def pop_with_args
-#dog_breeds = ["Great Dane", "Golden Retriever", "Chihuaha", "Shiba Inu"]
-#dog_breeds.pop(2)
-#end
